028b03d99a44b9a21f0938a27ae85322955adfcd
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-11 22:21:00 +0500

    Initial Commit

---
+++ EmptyPublisher/EmptyPublisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmptyPublisherApp.swift
+//  EmptyPublisher
+//
+//  Created by sss on 11.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct EmptyPublisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
