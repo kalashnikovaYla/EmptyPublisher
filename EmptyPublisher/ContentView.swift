ce4b51b37f32607e8252f68c4df3bfe63c5370d7
kalashnikovaYla <kalashnikova-yla@mail.ru>
2023-05-11 23:33:17 +0500

    Add

--- EmptyPublisher/ContentView.swift
+++ EmptyPublisher/ContentView.swift
@@ -6,18 +6,51 @@
 //
 
 import SwiftUI
+import Combine
 
 struct ContentView: View {
+    
+    @StateObject private var viewModel = EmptyFailurePublishersViewModel()
+    
+    var isVisible = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack(spacing: 20){
+            List(viewModel.dataToView, id: \.self) { item in
+                Text(item)
+            }
+            .font(.title)
         }
-        .padding()
+        .onAppear{
+            viewModel.fetch()
+        }
     }
 }
+
+class EmptyFailurePublishersViewModel: ObservableObject {
+    
+    @Published var dataToView: [String] = []
+    
+    let data = ["Value 1", "Value 2", "Value 3", nil, "Value 5", "Value 6"]
+    
+    func fetch() {
+        _ = data.publisher
+            .flatMap{ item -> AnyPublisher<String, Never> in
+                if let item = item {
+                    return Just(item)
+                        .eraseToAnyPublisher()
+                } else {
+                     return Empty(completeImmediately: true)
+                        .eraseToAnyPublisher()
+                }
+            }
+            .sink{ [unowned self] item in
+                dataToView.append(item)
+            }
+        
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
